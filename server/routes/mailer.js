9ee5569637b7cda19aef966de371145e39d843c2
Microzzx <janekitpk@gmail.com>
2023-03-06 18:01:30 +0700

    mailer env

--- server/routes/mailer.js
+++ server/routes/mailer.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "janekitpk10@gmail.com",
-    pass: "dxnwfgcvhoczawoh",
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
   },
 });
 
